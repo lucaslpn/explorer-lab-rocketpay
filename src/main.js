86a4177c1082d9e09ed82ee02463d4d7f737da3b
lucaslpn <85965293+lucaslpn@users.noreply.github.com>
2022-10-23 23:17:18 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -17,8 +17,6 @@ function setCardType(type) {
   ccLogo.setAttribute("src", `cc-${type}.svg`)
 }
 
-setCardType("mastercard")
-
 globalThis.setCardType = setCardType
 
 const securityCode = document.querySelector("#security-code")
